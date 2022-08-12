a71af8e68de984f45e54ce9813466190d11f0d60
Smr0303 <samarmohite7@gmail.com>
2022-08-12 12:08:16 +0530

    NftBox created

--- backend/deploy/99-update-frontend.js
+++ backend/deploy/99-update-frontend.js
@@ -3,18 +3,33 @@ const fs = require("fs")
 /*We update frontend using scripts */
 
 const frontEndContractFile = "../frontend-moralis/constants/networkMapping.json"
-
+const frontEndAbiLocation = "../frontend-moralis/constants/"
 module.exports = async () => {
   if (process.env.UPDATE_FRONTEND) {
     console.log("Updating frontend")
     await updateFrontendAddresses()
+    await updateAbi()
   }
+}
+
+/*To update the abi directly on frontend*/
+async function updateAbi() {
+  const nftMarketPlace = await ethers.getContract("nftMarketplace")
+  const basicNft = await ethers.getContract("BasicNft")
+  fs.writeFileSync(
+    `${frontEndAbiLocation}NftMarketPlace.json`,
+    nftMarketPlace.interface.format(ethers.utils.FormatTypes.json)//Check about this format in hardhat and ethers docs
+  );
+  fs.writeFileSync(
+    `${frontEndAbiLocation}BasicNft.json`,
+    basicNft.interface.format(ethers.utils.FormatTypes.json)
+  );
 }
 
 async function updateFrontendAddresses() {
   const nftMarketPlace = await ethers.getContract("nftMarketplace")
   const chainId = network.config.chainId.toString()
-  const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractFile, "utf8"));
+  const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractFile, "utf8"))
   if (chainId in contractAddresses) {
     if (!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketPlace.address)) {
       contractAddresses[chainId]["NftMarketplace"].push(nftMarketPlace.address)
---
+++ frontend-moralis/components/Nft-Box.js
@@ -0,0 +1,4 @@
+import React,{useState,useEffect} from 'react';
+export default function NFTBox(price ,nftAddress, tokenId, marketplaceAddress, seller){
+
+}
